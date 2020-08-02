91d757ec188bde479bbde1490c113661dfaf600f
betterIvy <aiwei-juicy@foxmail.com>
2020-08-03 00:34:45 +0800

    module01

--- wx_api/media/media.py
+++ wx_api/media/media.py
@@ -4,11 +4,11 @@ from wx_api.baseApi import BaseApi
 class Media(BaseApi):
 
    def upload(self,type=None,filename=None,content_type=None,**kwargs):
-       fp=
-       m=self.get_multipartm()
-       headers={
-           "content_type":m.content_type
-       }
-       self.source("media","upload").set_params(type=type).set_headers(headers).set_data(m).run()
+       # fp=
+       # m=self.get_multipartm()
+       # headers={
+       #     "content_type":m.content_type
+       # }
+       # self.source("media","upload").set_params(type=type).set_headers(headers).set_data(m).run()
        return self
 
